f89e4649a3a07cb82e0eab4d3ef68af404a9c474
Farid Mohd Ismail <farid@eonreality.com>
2019-10-20 16:22:54 +0800

    initial checkin with basic delegate working

--- delegate-01/Program.cs
+++ delegate-01/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using delegateclasslib;
 
 namespace delegate_01
 {
     class Program
     {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+        static int counter = 0;
+
         static void Main(string[] args)
         {
+            PopulateCartWithItems();
+            Console.WriteLine($"Total cost for the items (after discount) {cart.GenerateTotal(SubtotalAlert):C2}");
+            Console.ReadLine();
+        }
+
+        public static void SubtotalAlert(decimal subtotal)
+        {
+            counter++;
+            if (counter== 1)
+                Console.WriteLine($"{counter}: Sub-Total of items is '{subtotal:C2}'");
+            else if (counter ==2)
+                Console.WriteLine($"{counter}: Discount given '{subtotal:C2}'");
+        }
+
+        private static void PopulateCartWithItems()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cherio", Price = 14.90M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.15M });
+            cart.Items.Add(new ProductModel { ItemName = "Dragon Fruit", Price = 4.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Noodles", Price=1.50M });
         }
     }
 }
---
+++ delegateclasslib/ShoppingCartModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateclasslib
+{
+    public class ShoppingCartModel
+    {
+        //declare a delegate
+        public delegate void MentionDiscount(decimal subtotal);
+
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+
+        public decimal GenerateTotal(MentionDiscount mentionDiscount)
+        {
+            decimal subtotal = Items.Sum(x => x.Price);
+            decimal returnVal;
+
+            mentionDiscount(subtotal);
+
+            if (subtotal > 100)
+                returnVal = subtotal * 0.8M;
+            else if (subtotal > 50)
+                returnVal = subtotal * 0.85M;
+            else if (subtotal > 10)
+                returnVal = subtotal * 0.9M;
+            else
+                returnVal = subtotal;
+
+            mentionDiscount(subtotal - returnVal);
+
+            return returnVal;
+        }
+    }
+}
